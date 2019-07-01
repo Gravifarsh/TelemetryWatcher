ae128304035ac4465913576cf40eab09d2e68f28
Nick Trash <niktresh@mail.ru>
2019-07-01 22:51:19 +0300

    Working

--- datagenerator.cpp
+++ datagenerator.cpp
@@ -74,133 +74,99 @@ UDPDataGenerator::UDPDataGenerator(QObject* parent) :
             this, SLOT(processDatagram()));
 }
 
-typedef struct
-{
-    float time;
-    float temperature;
-    float pressure;
-    float height;
-} data_BMP280_t;
-
-typedef struct
-{
-    float time;
-    float accel[3];
-    float compass[3];
-    union
-    {
-    float gyro[3];
-    float quaternion[4];
-    };
-} data_MPU9255_t;
-
-typedef struct
-{
-    float time;
-    uint8_t	GlobalState;
-    uint8_t MPU9255_1;
-    uint8_t BMP280_1;
-    uint8_t MPU9255_2;
-    uint8_t BMP280_2;
-    uint8_t SD;
-    uint8_t	nRF;
-} system_state_t;
+uint8_t hash(uint8_t* data, size_t size) {
+    uint8_t retval = 0;
+    for(size_t i = 0; i < size; i++)
+        retval = retval ^ data[i];
+    return retval;
+}
 
 void UDPDataGenerator::tryParse() {
-    qDebug() << "Parsing";
+    //qDebug() << "Parsing";
+
+    qDebug() << "Buffer size: " << mBuffer.size();
 
     QMap<QString, QPointF> data;
-    int index, size;
 
-    size = sizeof(data_BMP280_t);
-    index = findPacket(0xFD, size);
-    if(index != -1) {
-        data_BMP280_t* packet = (data_BMP280_t*)(mBuffer.data() + index);
-        data["BMP_1_temp"] = {packet->time, packet->temperature};
-        data["BMP_1_press"] = {packet->time, packet->pressure};
-        data["BMP_1_height"] = {packet->time, packet->height};
+    data_BMP280_t data_BMP_1;
+    if(findPacket<data_BMP280_t>(0xFD, &data_BMP_1)) {
+        data["BMP_1_temp"] = {float(data_BMP_1.time) / 1000, data_BMP_1.temperature};
+        data["BMP_1_press"] = {float(data_BMP_1.time) / 1000, data_BMP_1.pressure};
+        data["BMP_1_height"] = {float(data_BMP_1.time) / 1000, data_BMP_1.height};
 
-        qDebug() << "BMP1 packet with time: " << packet->time;
-        qDebug() << "Temp: " << packet->temperature;
-        qDebug() << "Press: " << packet->pressure;
-        qDebug() << "Height: " << packet->height;
-
-        mBuffer.remove(index - 1, size + 2);
+        qDebug() << "BMP1 packet with time: " << data_BMP_1.time;
     }
 
-    size = sizeof(data_BMP280_t);
-    index = findPacket(0xFE, size);
-    if(index != -1) {
-        data_BMP280_t* packet = (data_BMP280_t*)(mBuffer.data() + index);
-        data["BMP_2_temp"] = {packet->time, packet->temperature};
-        data["BMP_2_press"] = {packet->time, packet->pressure};
-        data["BMP_2_height"] = {packet->time, packet->height};
+    data_BMP280_t data_BMP_2;
+    if(findPacket<data_BMP280_t>(0xFE, &data_BMP_2)) {
+        data["BMP_2_temp"] = {float(data_BMP_2.time) / 1000, data_BMP_2.temperature};
+        data["BMP_2_press"] = {float(data_BMP_2.time) / 1000, data_BMP_2.pressure};
+        data["BMP_2_height"] = {float(data_BMP_2.time) / 1000, data_BMP_2.height};
 
-        qDebug() << "BMP2 packet with time: " << packet->time;
-        qDebug() << "Temp: " << packet->temperature;
-        qDebug() << "Press: " << packet->pressure;
-        qDebug() << "Height: " << packet->height;
-
-        mBuffer.remove(index - 1, size + 2);
+        qDebug() << "BMP2 packet with time: " << data_BMP_2.time;
     }
 
-    size = sizeof(data_MPU9255_t);
-    index = findPacket(0xFA, size);
-    if(index != -1) {
-        data_MPU9255_t* packet = (data_MPU9255_t*)(mBuffer.data() + index);
-        data["MPU_1_accelX"] = {packet->time, packet->accel[0]};
-        data["MPU_1_accelY"] = {packet->time, packet->accel[1]};
-        data["MPU_1_accelZ"] = {packet->time, packet->accel[2]};
+    data_MPU9255_t data_MPU_1;
+    if(findPacket<data_MPU9255_t>(0xFA, &data_MPU_1)) {
+        data["MPU_1_accelX"] = {float(data_MPU_1.time) / 1000, data_MPU_1.accel[0]};
+        data["MPU_1_accelY"] = {float(data_MPU_1.time) / 1000, data_MPU_1.accel[1]};
+        data["MPU_1_accelZ"] = {float(data_MPU_1.time) / 1000, data_MPU_1.accel[2]};
 
-        data["MPU_1_gyroX"] = {packet->time, packet->gyro[0]};
-        data["MPU_1_gyroY"] = {packet->time, packet->gyro[1]};
-        data["MPU_1_gyroZ"] = {packet->time, packet->gyro[2]};
+        data["MPU_1_gyroX"] = {float(data_MPU_1.time) / 1000, data_MPU_1.gyro[0]};
+        data["MPU_1_gyroY"] = {float(data_MPU_1.time) / 1000, data_MPU_1.gyro[1]};
+        data["MPU_1_gyroZ"] = {float(data_MPU_1.time) / 1000, data_MPU_1.gyro[2]};
 
-        data["MPU_1_compassX"] = {packet->time, packet->compass[0]};
-        data["MPU_1_compassY"] = {packet->time, packet->compass[1]};
-        data["MPU_1_compassZ"] = {packet->time, packet->compass[2]};
+        data["MPU_1_compassX"] = {float(data_MPU_1.time) / 1000, data_MPU_1.compass[0]};
+        data["MPU_1_compassY"] = {float(data_MPU_1.time) / 1000, data_MPU_1.compass[1]};
+        data["MPU_1_compassZ"] = {float(data_MPU_1.time) / 1000, data_MPU_1.compass[2]};
 
-        mBuffer.remove(index - 1, size + 2);
+        qDebug() << "MPU 1 Packet with time: " << data_MPU_1.time;
     }
 
-    size = sizeof(data_MPU9255_t);
-    index = findPacket(0xFB, size);
-    if(index != -1) {
-        data_MPU9255_t* packet = (data_MPU9255_t*)(mBuffer.data() + index);
-        data["MPU_2_accelX"] = {packet->time, packet->accel[0]};
-        data["MPU_2_accelY"] = {packet->time, packet->accel[1]};
-        data["MPU_2_accelZ"] = {packet->time, packet->accel[2]};
+    data_MPU9255_t data_MPU_2;
+    if(findPacket<data_MPU9255_t>(0xFB, &data_MPU_2)) {
+        data["MPU_2_accelX"] = {float(data_MPU_2.time) / 1000, data_MPU_2.accel[0]};
+        data["MPU_2_accelY"] = {float(data_MPU_2.time) / 1000, data_MPU_2.accel[1]};
+        data["MPU_2_accelZ"] = {float(data_MPU_2.time) / 1000, data_MPU_2.accel[2]};
 
-        data["MPU_2_gyroX"] = {packet->time, packet->gyro[0]};
-        data["MPU_2_gyroY"] = {packet->time, packet->gyro[1]};
-        data["MPU_2_gyroZ"] = {packet->time, packet->gyro[2]};
+        data["MPU_2_gyroX"] = {float(data_MPU_2.time) / 1000, data_MPU_2.gyro[0]};
+        data["MPU_2_gyroY"] = {float(data_MPU_2.time) / 1000, data_MPU_2.gyro[1]};
+        data["MPU_2_gyroZ"] = {float(data_MPU_2.time) / 1000, data_MPU_2.gyro[2]};
 
-        data["MPU_2_compassX"] = {packet->time, packet->compass[0]};
-        data["MPU_2_compassY"] = {packet->time, packet->compass[1]};
-        data["MPU_2_compassZ"] = {packet->time, packet->compass[2]};
+        data["MPU_2_compassX"] = {float(data_MPU_2.time) / 1000, data_MPU_2.compass[0]};
+        data["MPU_2_compassY"] = {float(data_MPU_2.time) / 1000, data_MPU_2.compass[1]};
+        data["MPU_2_compassZ"] = {float(data_MPU_2.time) / 1000, data_MPU_2.compass[2]};
 
-        mBuffer.remove(index - 1, size + 2);
+        qDebug() << "MPU 2 Packet with time: " << data_MPU_2.time;
     }
 
     riseData(data);
 }
 
-int UDPDataGenerator::findPacket(uint8_t flag, size_t size) {
-    int left = mBuffer.indexOf(char(flag));
+template<typename T>
+bool UDPDataGenerator::findPacket(uint8_t flag, T* retval) {
+    size_t size = sizeof(T);
+    int left = mBuffer.indexOf(char(flag)), right;
     while(left != - 1) {
-        int right = left;
+        right = left;
 
-        while((right - left - 1) < int(size) && (right != - 1))
+        while((right - left - 1) < (int(size) + 1) && (right != - 1))
             right = mBuffer.indexOf(char(flag), right + 1);
 
-        if((right - left - 1) == int(size)) {
-            return (left + 1);
+        if((right - left - 1) == int(size) + 1) {
+            uint8_t* data = (uint8_t*)mBuffer.data() + left + 2;
+            uint8_t packet_hash = mBuffer[left + 1];
+            uint8_t _hash = hash(data, size);
+
+            if(_hash == packet_hash) {
+                memcpy(retval, data, size);
+                mBuffer.remove(left, right);
+                return true;
+            }
         }
-        else {
-            left = mBuffer.indexOf(char(flag), left + 1);
-        }
+        left = mBuffer.indexOf(char(flag), left + 1);
     }
-    return -1;
+    return false;
 }
 
 void UDPDataGenerator::processDatagram() {
--- datagenerator.h
+++ datagenerator.h
@@ -49,7 +49,8 @@ private:
     QByteArray mBuffer;
     QUdpSocket mSocket;
 
-    int findPacket(uint8_t flag, size_t size);
+    template<typename T>
+    bool findPacket(uint8_t flag, T* retval);
     void tryParse();
 public:
     UDPDataGenerator(QObject* parent = 0);
@@ -58,5 +59,66 @@ public:
 public slots:
     void processDatagram();
 };
+
+typedef struct
+{
+    uint32_t time;
+    float temperature;
+    float pressure;
+    float height;
+} data_BMP280_t;
+
+typedef struct
+{
+    uint32_t time;
+    float accel[3];
+    float compass[3];
+    union
+    {
+    float gyro[3];
+    float quaternion[4];
+    };
+} data_MPU9255_t;
+
+typedef enum
+{
+    EMAP_STATE_START,
+    EMAP_STATE_READY,
+    EMAP_STATE_PAYLOAD,
+    EMAP_STATE_FALLING
+} EMAP_state_t;
+
+typedef struct
+{
+    uint32_t time;
+    EMAP_state_t GlobalState;
+    uint8_t MPU9255_1;
+    uint8_t BMP280_1;
+    uint8_t MPU9255_2;
+    uint8_t BMP280_2;
+    uint8_t SD;
+    uint8_t	nRF;
+    uint8_t GPS;
+} system_state_t;
+
+typedef struct
+{
+    uint32_t time;
+    float coords[3];
+} data_GPS_t;
+
+typedef struct
+{
+    uint32_t time;
+    uint16_t ch0;
+    uint16_t ch1;
+    uint16_t lux;
+} data_TSL_t;
+
+typedef struct
+{
+    uint32_t time;
+    uint32_t dist;
+} data_LIDAR_t;
 
 #endif // DATAGENERATOR_H
